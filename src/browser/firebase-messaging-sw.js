70201aa55ebb3390d9ded9fa7b700190311d83dd
Mark Corner <mcorner@gmail.com>
2017-07-06 23:07:53 -0400

    maybe this?

--- src/browser/firebase-messaging-sw.js
+++ src/browser/firebase-messaging-sw.js
@@ -17,9 +17,9 @@
 importScripts('https://www.gstatic.com/firebasejs/4.1.3/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/4.1.3/firebase-messaging.js');
 
-/* firebase.initializeApp({
-   'messagingSenderId': '85075801930' //TBD: get this from package.json
- });*/
+firebase.initializeApp({
+//   'messagingSenderId': '85075801930' //TBD: get this from package.json
+ });
  // Retrieve an instance of Firebase Messaging so that it can handle background
  // messages.
  const messaging = firebase.messaging();
