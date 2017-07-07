f1f8db667f41915bdd6ea5d5b649b3e7a60d0827
Mark Corner <mcorner@gmail.com>
2017-07-06 21:59:56 -0400

    new loader

--- www/browser/push.js
+++ www/browser/push.js
@@ -7,13 +7,49 @@
 
 var exec = cordova.require('cordova/exec');
 
-const firebaseScript = document.createElement('script');
-firebaseScript.setAttribute('src','https://www.gstatic.com/firebasejs/4.1.3/firebase-app.js');
-document.getElementsByTagName('head')[0].appendChild(firebaseScript);
+var load = (function() {
+  // Function which returns a function: https://davidwalsh.name/javascript-functions
+  function _load(tag) {
+    return function(url) {
+      // This promise will be used by Promise.all to determine success or failure
+      return new Promise(function(resolve, reject) {
+        var element = document.createElement(tag);
+        var parent = 'body';
+        var attr = 'src';
 
-const firebaseMessagingScript = document.createElement('script');
-firebaseMessagingScript.setAttribute('src','https://www.gstatic.com/firebasejs/4.1.3/firebase-messaging.js');
-document.getElementsByTagName('head')[0].appendChild(firebaseMessagingScript);
+        // Important success and error for the promise
+        element.onload = function() {
+          resolve(url);
+        };
+        element.onerror = function() {
+          reject(url);
+        };
+
+        // Need to set different attributes depending on tag type
+        switch(tag) {
+          case 'script':
+            element.async = true;
+            break;
+          case 'link':
+            element.type = 'text/css';
+            element.rel = 'stylesheet';
+            attr = 'href';
+            parent = 'head';
+        }
+
+        // Inject into document to kick off loading
+        element[attr] = url;
+        document[parent].appendChild(element);
+      });
+    };
+  }
+
+  return {
+    css: _load('link'),
+    js: _load('script'),
+    img: _load('img')
+  }
+})();
 
 
 /**
@@ -59,9 +95,14 @@ var PushNotification = function(options) {
         channel.port1.onmessage = function(event) {
             that.emit('notification', event.data);
         };
-        const messaging = firebase.messaging();
 
-        messaging.requestPermission().then(function() {
+        let messaging = null;
+        load.js('https://www.gstatic.com/firebasejs/4.1.3/firebase-app.js').then(function(){
+          return load.js('https://www.gstatic.com/firebasejs/4.1.3/firebase-messaging.js');
+        }).then(function(){
+          messaging = firebase.messaging();
+          return messaging.requestPermission();
+        }).then(function() {
           console.log('Notification permission granted.');
           // TODO(developer): Retrieve an Instance ID token for use with FCM.
           // ...
