cebc7b3886223a64d891714e765e238633cb9b1a
Mark Corner <mcorner@gmail.com>
2017-07-11 15:03:50 -0400

    clicks

--- src/browser/firebase-messaging-sw.js
+++ src/browser/firebase-messaging-sw.js
@@ -55,3 +55,25 @@ messaging.setBackgroundMessageHandler(function(event) {
 self.addEventListener('message', function(event) {
   messageChannel = event;
 });
+
+self.addEventListener('notificationclick', function(event) {
+    console.log(event);
+    let url = 'http://markdcorner.com';
+    event.notification.close(); // Android needs explicit close.
+    event.waitUntil(
+        clients.matchAll({type: 'window'}).then( windowClients => {
+            // Check if there is already a window/tab open with the target URL
+            for (var i = 0; i < windowClients.length; i++) {
+                var client = windowClients[i];
+                // If so, just focus it.
+                if (client.url === url && 'focus' in client) {
+                    return client.focus();
+                }
+            }
+            // If not, then open the target URL in a new window/tab.
+            if (clients.openWindow) {
+                return clients.openWindow(url);
+            }
+        })
+    );
+});
